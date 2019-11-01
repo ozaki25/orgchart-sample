212c03ba770127b1aadb7fe52be6737530ce329a
ozaki <fs65ugumori@yahoo.co.jp>
2019-11-01 16:26:54 +0900

    なんか不安定だけどchartのデータを引数で

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Chart from './Chart';
 
+const data1 = {
+  nodeBinding: {
+    field_0: 'name',
+  },
+  nodes: [
+    { id: 1, name: 'Amber McKenzie' },
+    { id: 2, pid: 1, name: 'Ava Field' },
+    { id: 3, pid: 1, name: 'Peter Stevens' },
+  ],
+};
+
 function App() {
   return (
     // Chartでスクロールがブロックされるから左右に余白あけた
     <div style={{ margin: '0 30px' }}>
       <p>いろんなところにチャートを出してみる</p>
-      <Chart />
-      <p>いろんなところにチャートを出してみる</p>
-      <Chart />
-      <p>いろんなところにチャートを出してみる</p>
-      <Chart />
+      <Chart data={data1} />
     </div>
   );
 }
--- src/Chart.js
+++ src/Chart.js
@@ -1,18 +1,9 @@
 import React, { useEffect, useRef } from 'react';
 
-function Chart() {
+function Chart({ data }) {
   const ref = useRef(null);
   const showChart = () => {
-    new window.OrgChart(ref.current, {
-      nodeBinding: {
-        field_0: 'name',
-      },
-      nodes: [
-        { id: 1, name: 'Amber McKenzie' },
-        { id: 2, pid: 1, name: 'Ava Field' },
-        { id: 3, pid: 1, name: 'Peter Stevens' },
-      ],
-    });
+    new window.OrgChart(ref.current, data);
   };
 
   // コンポーネント描画時に一度だけ実行される
